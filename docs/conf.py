cd0ea4585fcd093562e40e5e6d918d6fa8000172
Emma Clegg <137316374+emmajclegg@users.noreply.github.com>
2025-01-28 18:08:17 -0500

    Update conf.py
    "IATI Validator" references updated for IATI Publisher

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# IATI Validator documentation build configuration file,
+# IATI Publisher documentation build configuration file,
 # created by sphinx-quickstart on Wed Nov  2 14:17:45 2016.
 #
 # This file is execfile()d with the current directory set to its
@@ -56,7 +56,7 @@ source_suffix = ['.rst']
 master_doc = 'index'
 
 # General information about the project.
-project = 'IATI Validator Docs'
+project = 'IATI Publisher Docs'
 copyright = '2024 United Nations Development Programme, on behalf of the IATI Secretariat'
 author = 'IATI Secretariat'
 
@@ -145,7 +145,7 @@ html_css_files = [
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
 #
-# html_title = 'IATI Validator Docs'
+# html_title = 'IATI Publisher Docs'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #
@@ -271,7 +271,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'sphinx.tex', 'IATI Validator Docs',
+    (master_doc, 'sphinx.tex', 'IATI Publisher Docs',
      'IATI Secretariat', 'manual'),
 ]
 
@@ -313,7 +313,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'sphinx', 'IATI Validator Docs',
+    (master_doc, 'sphinx', 'IATI Publisher Docs',
      [author], 1)
 ]
 
@@ -328,7 +328,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'sphinx', 'IATI Validator Docs',
+    (master_doc, 'sphinx', 'IATI Publisher Docs',
      author, 'sphinx', 'One line description of project.',
      'Miscellaneous'),
 ]
